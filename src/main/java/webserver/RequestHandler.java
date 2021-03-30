4008bb55197201098620a7dd3c18843eeab3bcf0
pbg0205 <pbg0205@naver.com>
2021-03-30 11:58:06 +0900

    feat : Complete requirement1
    - Request index.html page

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,9 +2,11 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,17 +24,16 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-            String line;
+            String line = br.readLine();
 
-            while(!"".equals(line = br.readLine())) {
-                if(line == null) {
-                    break;
-                }
-                log.debug("line : {}", line);
+            if(line == null) {
+                return;
             }
 
+            String url = HttpRequestUtils.getUrl(line);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
